3eaf43b3718e5c1a915da4c871752bd6ec80d4c0
Syed Ahamed <salearning01@gmail.com>
2025-11-25 22:21:38 +0530

    Daily Problems

---
+++ LeetCode75/283.MoveZeroes.js
@@ -0,0 +1,37 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+
+// Follow up: Could you minimize the total number of operations done?
+
+var moveZeroes = function (nums) {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log(`Iteration ${i}:`, nums);
+    if (nums[i] !== 0) {
+      console.log(`Iteration ${i}: Condition met`, { i, j });
+      console.log(`Iteration ${i}: Condition met 1 a`, nums[i], nums[j]);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j++;
+    }
+  }
+  return nums;
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12])); // [1,3,12,0,0]
+console.log(moveZeroes([0])); // [0]
